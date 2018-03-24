6a9344463b31d48cbd63773bb866a7945b9f09ef
ojus1 <surya.oju@gmail.com>
2018-03-24 17:40:26 +0530

    Added the cleaned and encoded dataset

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split,cross_val_score
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import AdaBoostClassifier
 
 from sklearn import linear_model
 
@@ -10,41 +11,44 @@ import matplotlib.pyplot as plt
 
 #Preprocessing, cleaning
 df = pd.read_csv("/home/ojus/Documents/classifiers_plot/credit.csv",header=None)
+df = df.drop([5])
 df = df.replace('?', value= np.nan)
 df = df.dropna(axis= 0, how= 'any')
 cat = [0,3,4,5,6,8,9,11,12,15]
 
 df = pd.get_dummies(df,columns= cat, drop_first= True)
+df.to_csv('cleaned_credit.csv')
 
 X = df.drop(columns= ['15_-'])
 Y = df[['15_-']]
 
 #Generating Scores for different classifiers
 logregScores = list()
-svmScores = list()
+forestScores = list()
 treeScores = list()
 
 for i in range(0,50) :
     
-    X_tr, X_ts, Y_tr, Y_ts = train_test_split(X, Y, test_size=0.33)
+    X_tr, X_ts, Y_tr, Y_ts = train_test_split(X, Y, test_size=0.25)
 
+    
     logreg = linear_model.LogisticRegression()
     logreg.fit(X_tr,Y_tr)
-    logregScores.append(logreg.score(X_ts,Y_ts))
+    logregScores.append(cross_val_score(logreg,X_ts,Y_ts).mean())
 
-    supVecC = SVC(C= 1.25, gamma = np.random.rand())
-    supVecC.fit(X_tr,Y_tr)
-    svmScores.append(supVecC.score(X_ts,Y_ts))
+    forest = AdaBoostClassifier(n_estimators = 100)
+    forestScores.append(cross_val_score(forest,X_ts,Y_ts).mean())
+    
     
     tree = DecisionTreeClassifier(random_state=0)
     tree.fit(X_tr,Y_tr)
-    treeScores.append(tree.score(X_ts,Y_ts))
+    treeScores.append(cross_val_score(tree,X_ts,Y_ts).mean())
 
 #Plotting the Scores
 fig,ax = plt.subplots()
 
 ax.plot(logregScores,'k--',label='Logistic Regression')
-ax.plot(svmScores,'k:',label='SVC')
+ax.plot(forestScores,'k:',label='Adaboost forest')
 ax.plot(treeScores,'k',label='Binary Decision Tree')
 ax.set_ylabel('Accuracy')
 ax.set_xlabel('Training No.')
